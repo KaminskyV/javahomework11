f6ce3c03f271a89ada3b96e62ee61e7de4bd188f
Kaminsky <kamwlad2010@gmail.com>
2023-02-05 19:04:25 +0400

    main

---
+++ src/main/java/ru/netology/main.java
@@ -0,0 +1,7 @@
+package ru.netology;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public static void main(String[] args) {
+
+    }
+}
